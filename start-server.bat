c7278228ab2c2d02cdd04b98a95c5fdae90127ef
dhafin5858 <dhafin5858@gmail.com>
2025-12-23 02:24:58 +0700

    first commit

--- start-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting local web server for portfolio...
-echo.
-echo Open your browser and navigate to:
-echo http://localhost:8000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-python -m http.server 8000
